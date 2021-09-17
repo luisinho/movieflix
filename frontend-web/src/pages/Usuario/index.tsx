7df254b1f1afdc9cf9f51a9e267c685262322b89
luis3977@gmail.com <luis3977@gmail.com>
2021-09-17 00:07:41 -0300

    implementation validation fields.

--- frontend-web/src/pages/Usuario/index.tsx
+++ frontend-web/src/pages/Usuario/index.tsx
@@ -30,49 +30,78 @@ const Usuario = () => {
                 <div className="margin-bottom-30">
                     <input
                         type="text"
-                        className="form-control user-input"
+                        className={`form-control login-input ${errors.name ? 'is-invalid' : ''}`}
                         placeholder="Nome"
-                        {...register("name")}
+                        {...register("name", { required: "Campo nome obrigatório." })}
                     />
+                    {errors.name && (
+                        <div className="invalid-feedback d-block">
+                            {errors.name.message}
+                        </div>
+                    )}
                 </div>
 
                 <div className="margin-bottom-30">
                     <input
                         type="email"
-                        className="form-control user-input"
+                        className={`form-control login-input ${errors.email ? 'is-invalid' : ''}`}
                         placeholder="Email"
-                        {...register("email")}
+                        {...register("email", {
+                            required: "Campo email obrigatório",
+                            pattern: {
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                message: "Email inválido"
+                            }
+                        })}
                     />
+                    {errors.email && (
+                        <div className="invalid-feedback d-block">
+                            {errors.email.message}
+                        </div>
+                    )}
                 </div>
 
                 <div className="d-flex flex-row justify-content-between margin-bottom-30">
                     <div className="user-input-password">
                         <input
                             type="password"
-                            className="form-control user-input"
+                            className={`form-control login-input ${errors.password ? 'is-invalid' : ''}`}
                             placeholder="Digite aqui a Senha"
-                            {...register("password")}
+                            {...register("password", { required: "Campo Senha obrigatório." })}
                         />
+                        {errors.password && (
+                            <div className="invalid-feedback d-block">
+                                {errors.password.message}
+                            </div>
+                        )}
                     </div>
 
                     <div className="user-input-password">
                         <input
                             type="password"
-                            className="form-control user-input"
+                            className={`form-control login-input ${errors.repeatPassword ? 'is-invalid' : ''}`}
                             placeholder="Repita aqui a Senha"
-                            {...register("repeatPassword")}
+                            {...register("repeatPassword", { required: "Campo Repita aqui a Senha obrigatório." })}
                         />
+                        {errors.repeatPassword && (
+                            <div className="invalid-feedback d-block">
+                                {errors.repeatPassword.message}
+                            </div>
+                        )}
                     </div>
 
                 </div>
 
                 <div className="user-btn">
-                    <div className="user-btn-register-back">
+
+                    <div className="p-1">
                         <ButtonBack />
                     </div>
-                    <div className="user-btn-register-back">
+
+                    <div className="p-1">
                         <ButtonRegister />
                     </div>
+
                 </div>
 
             </form>
