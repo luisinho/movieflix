08acd94b63d9626a5d5633451cfa88fa60203b73
luis3977@gmail.com <luis3977@gmail.com>
2021-12-24 20:47:22 -0200

    implemented texts and home screen button.

--- frontend-mobile/src/pages/Home/index.tsx
+++ frontend-mobile/src/pages/Home/index.tsx
@@ -1,17 +1,47 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import drawHome from '../../assets/draw-home.png';
-import { theme } from '../../styles';
+import arrow from '../../assets/arrow.png';
+import { theme, text } from '../../styles';
 
 const Home: React.FC = () => {
+
+    const navigation = useNavigation();
+
+    const catalog = 'Catalog';
 
     return (
 
         <View style={theme.container}>
 
             <View style={theme.card}>
+
                 <Image source={drawHome} style={theme.draw} />
+
+                <View style={theme.textContainer}>
+
+                    <Text style={text.bold}>Avalie filmes</Text>
+
+                    <Text style={text.regular}>Diga o que você achou do seu filme favorito.</Text>
+
+                </View>
+
+                <TouchableOpacity
+                    style={theme.primaryButton}
+                    activeOpacity={0.8}
+                    onPress={() => navigation.navigate(catalog as never, {} as never)}
+                >
+
+                    <Text style={text.primaryText}>FAZER LOGIN</Text>
+
+                    <View style={theme.arrowContainer}>
+                        <Image source={arrow} />
+                    </View>
+
+                </TouchableOpacity>
+
             </View>
 
         </View>
--- frontend-mobile/src/styles/index.ts
+++ frontend-mobile/src/styles/index.ts
@@ -1,33 +1,142 @@
 import { StyleSheet } from 'react-native';
 
 const colors = {
+
     white: "#FFFFFF",
+
     black: "#000000",
+
     mediumYellow: "#FFC700",
-    mediumGray: "#6C6C6C"
+
+    ligthYellow: "#937d22",
+
+    mediumGray: "#6C6C6C",
 };
 
-const theme = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        padding: 20
+const text = StyleSheet.create({
 
+    regular: {
+
+        fontSize: 18,
+
+        fontWeight: "400",
+
+        textAlign: "center",
+
+        color: colors.white,
+    },
+
+    bold: {
+
+        fontSize: 26,
+
+        fontWeight: "bold",
+
+        textAlign: "center",
+
+        color: colors.white,
+
+        marginBottom: 25,
+    },
+
+    primaryText: {
+
+        fontSize: 16,
+
+        fontWeight: "bold",
+
+        textTransform: "uppercase",
+
+        color: colors.black,
+
+        marginLeft: 55,
+    }
+});
+
+const theme = StyleSheet.create({
+
+    container: {
+
+        flex: 1,
+
+        justifyContent: "center",
+
+        alignItems: "center",
+
+        padding: 20,
     },
 
     card: {
+
         width: "100%",
+
         height: "100%",
-        backgroundColor: colors.white,
+
+        backgroundColor: colors.mediumGray,
+
         borderRadius: 20,
-        alignItems: "center"
+
+        shadowColor: colors.black,
+
+        shadowOffset: {
+
+            width: 0,
+
+            height: 2,
+        },
+
+        shadowOpacity: 0.25,
+
+        shadowRadius: 3.84,
+
+        alignItems: "center",
+
+        justifyContent: "space-around",
     },
 
     draw: {
         width: 313,
         height: 225,
-    }
+    },
+
+    textContainer: {
+
+        paddingHorizontal: 20,
+    },
+
+    primaryButton: {
+
+        width: 250,
+
+        height: 50,
+
+        backgroundColor: colors.mediumYellow,
+
+        borderRadius: 10,
+
+        flexDirection: "row",
+
+        justifyContent: "space-between",
+
+        alignItems: "center"
+    },
+
+    arrowContainer: {
+
+        width: 50,
+
+        height: 50,
+
+        backgroundColor: colors.ligthYellow,
+
+        borderTopRightRadius: 10,
+
+        borderBottomRightRadius: 10,
+
+        alignItems: "center",
+
+        justifyContent: "center",
+    },
 });
 
-export { colors, theme };
+export { colors, text, theme };
