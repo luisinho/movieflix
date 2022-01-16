5d03662628039ea7eb3ae12a23e5927c9be054c2
luis3977@gmail.com <luis3977@gmail.com>
2022-01-16 20:27:25 -0200

    configuration api localhost.

--- frontend-mobile/src/services/api.tsx
+++ frontend-mobile/src/services/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
-const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'https://luis-movieflix.herokuapp.com';
+const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+// const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'https://luis-movieflix.herokuapp.com';
 
 export const Api = axios.create({
     baseURL: BASE_URL
